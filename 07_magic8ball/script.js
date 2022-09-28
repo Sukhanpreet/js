0d18f5d66171300cc1d49f1b4b1dc5833ee2e547
Sukhanpreet <sukhanpreet356@gmail.com>
2022-09-28 12:46:56 +0530

    Script updated

--- 07_magic8ball/script.js
+++ 07_magic8ball/script.js
@@ -1,25 +1,33 @@
 console.log('Script working');
 
 const ques= document.getElementById('ques');
+const err=document.querySelector('.err');
 const magicBall=document.getElementById('ball');
 
 let ball={
     advice:['It is certain.','It is decidedly so.','Without a doubt.','Yes definitely.'
     ,'You may rely on it.','As I see it, yes.','Most likely.','Outlook good.','Yes.','Signs point to yes.','Reply hazy, try again.','Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.','Don\'t count on it.','My reply is no.','My sources say no.','Outlook not so good.','Very doubtful.'],
-
+    ballState: false,
     getAdvice: function(ques){
-        if(ques.value!=''){
-            return this.advice[Math.floor(Math.random()*this.advice.length)];
+        if(!this.ballState){
+            this.ballState=true;
+            let ans=this.advice[Math.floor(Math.random()*this.advice.length)];
+            magicBall.innerHTML=ans;
+        }else{
+           ques.value='';
+           magicBall.innerHTML='8'; 
+           this.ballState=false;
         }
-        else{
-            return 'ask something';
-        }
+        
     }
 
 }
 
 magicBall.addEventListener('click',function(){
-    let ans=ball.getAdvice(ques);
-    console.log(ans);
-    magicBall.innerHTML=ans;
+    if(ques.value==''){
+        alert('You have to ASK something to get the answer');
+    } else{
+        ball.getAdvice(ques);   
+    }
+    
 });
