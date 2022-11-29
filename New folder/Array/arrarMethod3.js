05b190d6196a67dfe8f40ae2874aee622258eeed
Sukhanpreet <sukhanpreet356@gmail.com>
2022-11-29 19:57:42 +0530

    arrMethod3 created

---
+++ New folder/Array/arrarMethod3.js
@@ -0,0 +1,17 @@
+const arr1=[1,6,2,4,5,1,9,3];
+
+console.log(arr1);
+const arr2=arr1.sort((a,b)=>b-a);
+
+console.log(arr2);
+
+
+
+let fname='sukhan';
+let age=24;
+
+let obj={fname,age};
+
+console.log(obj);
+
+
